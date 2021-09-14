70bccd399e7517d06fd752170a306d9784ee4e0e
Frankle97 <pairpapa@gmail.com>
2021-09-14 09:39:26 +0900

    Chapter1. IoC 개념과 ApplicationContext

--- src/main/java/user/dao/DaoFactory.java
+++ src/main/java/user/dao/DaoFactory.java
@@ -1,9 +1,30 @@
 package user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
-    public UserDao userDao () {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
+    @Bean
+    public UserDao userDao() {
+        ConnectionMaker connectionMaker = getConnection();
+        return new UserDao(connectionMaker);
+    }
+
+    public UserDao accountDao() {
+        ConnectionMaker connectionMaker = getConnection();
         UserDao userDao = new UserDao(connectionMaker);
         return userDao;
+    }
+
+    public UserDao messageDao() {
+        ConnectionMaker connectionMaker = getConnection();
+        UserDao userDao = new UserDao(connectionMaker);
+        return userDao;
+    }
+
+    @Bean
+    public ConnectionMaker getConnection() {
+        return new DConnectionMaker();
     }
 }
