857e572b9455deaa62fd27c598bd4c2290c4e709
Frankle97 <pairpapa@gmail.com>
2021-09-16 15:40:36 +0900

    Chapter1. IoC와 DI/DL

--- src/main/java/user/dao/UserDao.java
+++ src/main/java/user/dao/UserDao.java
@@ -15,9 +15,7 @@ public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
         Connection conn = connectionMaker.makeConnection();
 
-        PreparedStatement ps = conn.prepareStatement(
-                "insert into users(id, name, password) values (?,?,?)"
-        );
+        PreparedStatement ps = conn.prepareStatement("insert into users(id, name, password) values (?,?,?)");
         ps.setString(1 ,user.getId());
         ps.setString(2 ,user.getName());
         ps.setString(3 ,user.getPassword());
