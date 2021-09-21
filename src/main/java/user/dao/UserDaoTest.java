51c610773db14c087217f381a802633d3115675e
Frankle97 <pairpapa@gmail.com>
2021-09-21 12:13:36 +0900

    Chapter1. DI, 의존성 XML을 통한 관리
    - 생성자 / 수정자 주입 방법
    - XML 외부에서 값 세팅을 통해 런타임 시 의존 관계 주입

--- src/main/java/user/dao/UserDaoTest.java
+++ src/main/java/user/dao/UserDaoTest.java
@@ -1,6 +1,7 @@
 package user.dao;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import user.domain.User;
 
@@ -9,6 +10,7 @@ import java.sql.SQLException;
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
